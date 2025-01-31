ab27d7992a2087d87556c459873402483e5df285
Rosco Kalis <roscokalis@gmail.com>
2025-01-31 18:45:54 +0100

    Add version string in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,7 @@ export default function Home() {
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        2024-01-31
       </footer>
     </div>
   );
